afbbfcfcbab7d856916c569ae4b13f448220708e
ranasauravsingh <ranasaurav121@gmail.com>
2024-09-08 16:04:51 +0530

    [Configure][MiddleWare] configured middleware for auth guard api calling

---
+++ middleware/authGuard.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+
+import { handleError } from "../_helpers/common_helper.js";
+
+// MIDDLEWARE
+const authGuard = async (req, res, next) => {
+	console.log("Middle Ware working");
+
+	const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+	try {
+
+		const token = req?.cookies?.token;
+		if (!token) {
+			return res.status(401).json({
+				message: "User not authenticated.",
+				success: false,
+			});
+		}
+
+		const decode = await jwt.verify(token, JWT_SECRET);
+
+		if (!decode) {
+			return res.status(401).json({
+				message: "Invalid token.",
+				success: false,
+			});
+		}
+
+		req.id = decode?.userID;
+		next();
+	} catch (err) {
+		handleError(res, err);
+	}
+};
+
+export default authGuard;
