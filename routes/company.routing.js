02bb45412bbbc87d89e81eeab6e6335c4e91f731
ranasauravsingh <ranasaurav121@gmail.com>
2024-10-20 14:35:53 +0530

    [Refactor][Company] integrated single upload from multer in company routing

--- routes/company.routing.js
+++ routes/company.routing.js
@@ -10,12 +10,13 @@ import {
 
 //? MiddleWare
 import authGuard from "../middleware/authGuard.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.route("/register").post(authGuard, companyRegister);
 router.route("/get").get(authGuard, getCompany);
 router.route("/get/:id").get(authGuard, getCompanyById);
-router.route("/update/:id").put(authGuard, companyUpdate);
+router.route("/update/:id").put(authGuard, singleUpload, companyUpdate);
 
 export default router;
