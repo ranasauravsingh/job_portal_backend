5e5e56d2f696e26e039928ae7f80c5c577316b23
ranasauravsingh <ranasaurav121@gmail.com>
2024-10-20 14:36:34 +0530

    [Refactor][Users] integrated single upload from multer in user routing

--- routes/user.routing.js
+++ routes/user.routing.js
@@ -10,12 +10,13 @@ import {
 
 //? MiddleWare
 import authGuard from "../middleware/authGuard.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.route("/login").post(userLogin);
-router.route("/register").post(userRegistration);
-router.route("/logout").post(authGuard, userLogout);
-router.route("/update-profile").post(authGuard, userUpdateProfile);
+router.route("/register").post(singleUpload, userRegistration);
+router.route("/logout").get(userLogout);
+router.route("/update-profile").post(authGuard, singleUpload, userUpdateProfile);
 
 export default router;
