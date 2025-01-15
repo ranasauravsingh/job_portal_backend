4697cbc4ca354464489138d228747b5ae3984b0c
ranasauravsingh <ranasaurav121@gmail.com>
2025-01-15 17:56:33 +0530

    [Refactor][Common] updated error message for fcm notification api call

--- _helpers/common_helper.js
+++ _helpers/common_helper.js
@@ -31,7 +31,7 @@ export const sendFcmNotification = async (fcmToken, notificationPayload) => {
 		return fcmResponseData?.data;
 	} catch (error) {
 		console.error(
-			"Error sending message:",
+			"Error FCM message:",
 			error?.response?.data || error?.message
 		);
 		throw error;
